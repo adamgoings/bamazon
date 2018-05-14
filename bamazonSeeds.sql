fde319f7449d24f6cbf60d12d580387aac900f70
Adam Goings <adamgoings@Adams-MacBook-Pro.local>
2018-05-13 22:05:14 -0400

    schema and seeds file-working on js now

---
+++ bamazonSeeds.sql
@@ -0,0 +1,13 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Spalding Basketball","Sports", 55.00, 100),
+("Wilson Football","Sports", 35.00, 50),
+("Taylor Made Irons(3-PW)", "Sports", 1000.00, 15),
+("Sectional Sofa", "Home", 890.00, 5),
+("Decorative Lamp", "Home", 150.00, 35),
+("Area Rug", "Home", 300.00, 25),
+("Wiper Blades", "Automotive", 85.00, 75),
+("Washer Fluid", "Automotive", 3.00, 300),
+("Xbox Controller", "Video Games", 65.00, 200),
+("PS4 Controller", "Video Games", 65.00, 250);
---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(50) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  price DECIMAL (10,2) NOT NULL,
+  stock_quantity INTEGER(11) NOT NULL,
+
+  PRIMARY KEY (item_id)
+);
